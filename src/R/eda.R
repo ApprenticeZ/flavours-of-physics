01fb2e4665a27c3a73faea961d131d02531c27d8
ApprenticeZ <rookiecoder.z@gmail.com>
2015-11-07 16:35:09 +0800

    data preprocessing

--- src/R/eda.R
+++ src/R/eda.R
@@ -4,16 +4,38 @@ setwd('/media/zhang/01CE0A9C75603BA0/pg-course/kdd/tau-mu/')
 source('./src/R/utils.R')
 
 train <- read.csv('data/training.csv')
-
-# target variable
-y <- train$signal
-
-# remove the last 5 features
-train <- train[,2:46]
-summary(train)
+test <- read.csv('data/test.csv')
 
 # feature normalization
-ntrain <- apply(train, 2, featNorm)
-summary(ntrain)
+ntrain <- apply(train[,c(-1,-49)], 2, featNorm)
+dtrain <- data.frame(train[,c(1,49)],ntrain)
+rm(ntrain)
+colnames(dtrain)[1:2] <- c('id','signal')
 
-saveHist(ntrain,'./images/hist')
+strain <- apply(train[,c(-1,-49)], 2, featScale)
+sdtrain <- data.frame(train[,c(1,49)],strain)
+rm(strain)
+colnames(sdtrain)[1:2] <- c('id','signal')
+
+ntest <- apply(test[,-1],2,featNorm)
+dtest <- data.frame(test[,1],ntest)
+rm(ntest)
+colnames(dtest)[1] <- 'id'
+write.csv(dtest, './data/ntest.csv', quote=FALSE, row.names=FALSE)
+
+stest <- apply(test[,-1],2,featScale)
+sdtest <- data.frame(test[,1],stest)
+rm(stest)
+colnames(sdtest)[1] <- 'id'
+write.csv(sdtest, './data/stest.csv', quote=FALSE, row.names=FALSE)
+
+
+saveHist(dtrain[,2:51],'./images/hist')
+saveHist(train[,2:51],'./images/raw-hist')
+
+saveScatter(train[,c(-1,-49)],train$signal,'./images/raw-scatter')
+saveScatter(sdtrain[,c(-1,-2)],sdtrain$signal,'./images/scale-hist')
+
+saveBox(train[,c(-1,-49)],train$signal,'./images/raw-box')
+saveBox(dtrain[,c(-1,-2)],dtrain$signal,'./images/box')
+saveBox(sdtrain[,c(-1,-2)],sdtrain$signal,'./images/scale-box')
--- src/R/utils.R
+++ src/R/utils.R
@@ -4,6 +4,12 @@ featNorm <- function(x){
     return ((x-mean(x))/sd(x))
 }
 
+featScale <- function(x){
+  minx <- min(x)
+  maxx <- max(x)
+  return ((x-minx)/(maxx-minx)+minx)
+}
+
 saveHist <- function(trainMat,path){
     nFeature <- dim(trainMat)[2]
     featName  <- colnames(trainMat)
@@ -26,6 +32,21 @@ saveScatter <- function(trainMat,target,path='./images/scatter'){
         p <- ggplot(dat, aes_string(x=featName[i], colour='signal')
                     ,xlab=featName[i],ylab='density'
                     ,main=paste(featName[i], '-signal scatter plot')) + geom_density()
+        ggsave(file=filename,p)
+        rm(p)
+    }
+}
+
+saveBox <- function(trainMat,target,path='./images/box'){
+    nFeature <- dim(trainMat)[2]
+    featName  <- colnames(trainMat)
+    for (i in 1:nFeature){
+        print(featName[i])
+        filename <- paste0(path,'/',featName[i],'.png')
+        dat <- data.frame(trainMat[,i],as.factor(target))
+        colnames(dat) <- c(featName[i],'signal')
+        p <- ggplot(dat, aes_string(x='signal', y=featName[i], colour='signal')
+                    ,xlab=featName[i],main=paste(featName[i], '-signal boxplot')) + geom_boxplot()
         ggsave(file=filename,p)
         rm(p)
     }
